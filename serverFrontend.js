1706f07ad9ee122965b1ea03a6c694e9112d598e
Rostislav Klein <rostislav.klein@dhl.com>
2018-04-06 00:41:06 +0200

    Reverting previous stupid step.

--- serverFrontend.js
+++ serverFrontend.js
@@ -1,12 +1,5 @@
 const express = require('express')
 const next = require('next')
-const cors = require('cors');
-const bodyParser = require("body-parser");
-var cookieParser = require('cookie-parser')
-
-var UserController = require('./api/user/UserController');
-var FormController = require('./api/form/FormController');
-var AuthController = require('./api/auth/AuthController');
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -15,19 +8,6 @@ const handle = app.getRequestHandler()
 app.prepare()
 .then(() => {
   const server = express()
-  var db = require('./api/db');
-  server.use(bodyParser.urlencoded({
-      extended: true
-  }));
-  server.use(cookieParser())
-  server.use(cors());
-  server.options('*', cors());
-
-  
-  server.use('/api/users', UserController);
-  server.use('/api/form', FormController);
-  server.use('/api/auth', AuthController);
-
 
   server.get('/q/:id', (req, res) => {
     const actualPage = '/form'
