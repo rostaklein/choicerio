3f687499d2108d6e60c7497674ce6b2d15482157
Rostislav Klein <rostislav.klein@dhl.com>
2018-04-06 16:50:03 +0200

    Separated API.

--- api/form/Question.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');  
-var QuestionSchema = new mongoose.Schema({  
-  title: String,
-  description: String,
-  order: Number
-});
-mongoose.model('Question', QuestionSchema);
-
-module.exports = mongoose.model('Question');
