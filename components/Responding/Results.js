75da58f20fff9a6be97ed9e1398cfc81f7b5f9f3
Rostislav Klein <rostislav.klein@dhl.com>
2018-04-15 14:10:28 +0200

    Comparing answers.

--- components/Responding/Results.js
+++ components/Responding/Results.js
@@ -2,17 +2,24 @@ import stylesheet from 'styles/responding/Welcome.scss';
 import { Link } from '../../routes';
 import Router from 'next/router';
 import { compare } from "../../constants";
-const example = {
-    "5acccd50ea701700048c1f82": 0,
-    "5acccd50ea701700048c1f83": 3,
-    "5acccd50ea701700048c1f84": 4,
-    "5acccd50ea701700048c1f85": 3,
-    "5acccd50ea701700048c1f86": 4
-  };
 const Results = (props) =>
 <article className="form-responding">
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-    {JSON.stringify(compare(props.answers, example))}
+    {props.form.candidates.map(candidate=>
+        <div>
+            <div className="candidate">{candidate.title}</div>
+            <div>
+                {props.form.candidateSubmissions[candidate._id] ?
+                    JSON.stringify(compare(props.answers, props.form.candidateSubmissions[candidate._id]))
+                    :
+                    <span>This candidate has no answers set yet.</span>
+                }
+            
+            </div>
+        </div>
+        
+    )}
+    {/* {JSON.stringify(compare(props.answers, example))} */}
 </article>
 
 export default Results;
